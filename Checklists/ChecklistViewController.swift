63719d93a0b825d4ac0f0e717423dc356706b0a9
ivantiu <ivankentiu@gmail.com>
2017-10-09 13:31:37 +0800

    added check accessory

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Ivan Ken Tiu on 09/10/2017.
+//  Copyright © 2017 FinalShift Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+}
+
